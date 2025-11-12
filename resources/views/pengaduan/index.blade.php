ac28215d3216872a5938f5ed94b92c9cc999df7e
Bayu23-star <bayukatonsa@gmail.com>
2025-11-12 19:55:50 +0700

    edit

--- resources/views/pengaduan/index.blade.php
+++ resources/views/pengaduan/index.blade.php
@@ -23,7 +23,7 @@
 
 <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('pengaduan.index')}}">&nbsp;&nbsp;&nbsp;&nbsp;Sistem Zulaikha</a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('pengaduan.index')}}">&nbsp;&nbsp;&nbsp;&nbsp;Sistem Pengaduan Sarpras</a>
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
                 <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign out</a>
