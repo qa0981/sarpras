9fa36d80bdfdcbc84c770bec4b10cfe5ee5c09f1
zulaikha <zulaikhaa.mutu@gmail.com>
2026-01-08 17:31:08 +0700

    update

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Register berhasil',
+            'user' => $user,
+        ], 201);
+    }
+}
